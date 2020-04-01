c10146574c713945fa298e544da072d580a21380
Morro Luarvik <dr.morro.l@gmail.com>
2020-04-01 17:08:10 +1000

    loading script

--- task1.js
+++ task1.js
@@ -1,11 +1,13 @@
-const minDiskCou = 1;
-const maxDiskCou = 10;
+console.log('Let\'s go');
+
+const MIN_DISK_COU = 1;
+const MAX_DISK_COU = 10;
 
 var diskCou = 5;
 var curPositions = 000111;
 var secretPositions = 111000;
 
-if (diskCou < minDiskCou || diskCou > maxDiskCou)
+if (diskCou < MIN_DISK_COU || diskCou > MAX_DISK_COU)
     console.error('Количкество дисков должно быть от ' + minDiskCou + 'до' + maxDiskCou);
 
 console.log('Let\'s go');
